e8af9731dd3540ce38293bd85b59005500322cc3
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-01 22:08:59 +0100

    first commit for modern react app

---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./brand.css"
+
+const brand = () => {
+  return (
+    <div>
+      Brand
+    </div>
+  )
+}
+
+export default brand
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./navbar.css"
+
+const NavBar = () => {
+  return (
+    <div>
+      NAvbar
+    </div>
+  )
+}
+
+export default NavBar
