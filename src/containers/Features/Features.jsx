2c3d2e32f65918387d5c18bb244d40e760034323
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-05 21:02:41 +0100

    done with Features Mark-up

--- src/containers/Features/Features.jsx
+++ src/containers/Features/Features.jsx
@@ -1,12 +1,51 @@
 import React from 'react'
 import "./features.css"
+import { Feature } from '../../components'
+import { nanoid } from 'nanoid'
 
-const Feature = () => {
+const Features = () => {
+
+  const featureData = [
+    {
+      title: "Improving end distrusts instantly",
+      text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded."
+    },
+    {
+      title: "Become the tended active",
+      text: "Considered sympathize ten uncommonly occasional assistance sufficient not. Letter of on become he tended active enable to."
+    },
+    {
+      title: "Message or am nothing",
+      text: "Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address."
+    },
+    {
+      title: "Really boy law county",
+      text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
+    },
+  ]
+
+  const featureItems = featureData.map(featureArrItem => {
+    return <Feature 
+      key={nanoid()}
+      title={featureArrItem.title}
+      text={featureArrItem.text}
+    />
+  })
+
+  console.log(featureItems)
+
+
   return (
-    <div>
-      Feature
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features-heading">
+        <h1 className='gradient__text'>The Future is Now and You Just Need To realize it. Step into the Future Today & Make it Happen.</h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featureItems}
+      </div>
     </div>
   )
 }
 
-export default Feature
+export default Features
