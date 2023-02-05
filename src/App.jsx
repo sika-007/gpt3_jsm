91e026e96e71d8389bb1977546f46850039a39ef
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-05 21:04:00 +0100

    imports clean-up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Brand, CTA, Navbar } from './components';
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header} from "./containers"
 import "./App.css"
 
