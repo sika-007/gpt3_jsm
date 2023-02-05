b5c97566e15357a596da696de3d6dfbbeae1e7b2
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-05 22:08:45 +0100

    sliding animation added

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import ai from "../../assets/ai.png"
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='home'>
+    <div className='gpt3__header section__padding slide-top' id='home'>
       <div className='gpt3__header-content'>
 
         <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
