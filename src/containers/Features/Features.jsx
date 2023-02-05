ae022763635cb07753c51e3ef1225506bf530b08
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-05 22:08:11 +0100

    layout completed for Features container

--- src/containers/Features/Features.jsx
+++ src/containers/Features/Features.jsx
@@ -21,7 +21,7 @@ const Features = () => {
     {
       title: "Really boy law county",
       text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush."
-    },
+    }
   ]
 
   const featureItems = featureData.map(featureArrItem => {
@@ -31,8 +31,6 @@ const Features = () => {
       text={featureArrItem.text}
     />
   })
-
-  console.log(featureItems)
 
 
   return (
