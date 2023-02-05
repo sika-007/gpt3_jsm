519e82d8d6d5971b0b902783877d70f2348accee
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-05 15:02:14 +0100

    markup and styling

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Feature } from '../../components'
 import "./whatgpt3.css"
 
 const whatGPT = () => {
   return (
-    <div>
-      whatGpt
+    <div className='gpt3__whatgpt3 section__margin' id="wgpt3">
+      <div className='gpt3__whatgpt3-feature'>
+        <Feature 
+          title="What is GPT-3?"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature 
+          title="Chatbots"
+          text=""
+        />
+        <Feature 
+          title="Knowledgebase"
+          text=""
+        />
+        <Feature 
+          title="Education"
+          text=""
+        />
+      </div>
     </div>
   )
 }
