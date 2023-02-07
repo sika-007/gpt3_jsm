ad84c56e8d1c34884ed2ef2472bfc80057ebca58
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-07 18:44:13 +0100

    index.html customization

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import "./blog.css"
+import { Article } from '../../components'
+import { Blog01, Blog02, Blog03, Blog04, Blog05} from "./index"
+
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt3__blog section__padding'>
+      <div className='gt3__blog-heading'>
+        <h1 className="gradient__text">A lot is happening, 
+We are blogging about it.</h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article 
+            imageURL={Blog01}
+            date="Sept 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
+          />
+        </div>
+        <div className="gpt3__blog-container_groupA">
+          <Article 
+            imageURL={Blog02}
+            date="Sept 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
+          />
+          <Article 
+            imageURL={Blog03}
+            date="Sept 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
+          />
+          <Article 
+            imageURL={Blog04}
+            date="Sept 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
+          />
+          <Article 
+            imageURL={Blog05}
+            date="Sept 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
+          />
+        </div>
+      </div>
     </div>
   )
 }
