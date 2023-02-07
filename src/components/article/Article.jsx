d69ec9e4e50a3b9d8937a4f6b7e84eab8f48387e
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-07 18:40:53 +0100

    article component laid out and styled

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./article.css"
 
-const Article = () => {
+const Article = (props) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container_article'>
+      <div className="gpt3__blog-container_article-image">
+        <img src={props.imageURL} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{props.date}</p>
+          <h3>{props.title}</h3>
+        </div>
+        <p>Read full article</p>
+      </div>
     </div>
   )
 }
