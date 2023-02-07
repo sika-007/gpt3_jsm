6c99fe2b5de4a706d9993c13b1ae0cdf8e808033
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-07 23:14:52 +0100

    blog layout completed using css grid

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -6,7 +6,7 @@ import { Blog01, Blog02, Blog03, Blog04, Blog05} from "./index"
 const Blog = () => {
   return (
     <div className='gpt3__blog section__padding'>
-      <div className='gt3__blog-heading'>
+      <div className='gpt3__blog-heading'>
         <h1 className="gradient__text">A lot is happening, 
 We are blogging about it.</h1>
       </div>
@@ -18,7 +18,7 @@ We are blogging about it.</h1>
             title="GPT-3 and Open  AI is the future. Let us exlore how it is?" 
           />
         </div>
-        <div className="gpt3__blog-container_groupA">
+        <div className="gpt3__blog-container_groupB">
           <Article 
             imageURL={Blog02}
             date="Sept 26, 2021"
