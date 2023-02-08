92cecfff5bba8becbfb7a1fb9bd471549b898a8d
Nsikak Thomas <nsikakthomas102@gmail.com>
2023-02-08 11:35:19 +0100

    Footer section laid out

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import "./footer.css"
+import gpt3Logo from "../../assets/logo.svg"
 
 const Footer = () => {
   return (
-    <div>
-      Footer
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className='gradient__text'>Do you want to step in to the future before others?</h1>
+      </div>
+      <div className='gpt3__footer-btn'>
+        <button>Request Early Access</button>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={gpt3Logo} alt="" />
+          <p>Crechterwoord K12 182 DK Alknjkcb, All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <ul>
+            <li>Overons</li>
+            <li>Social Media</li>
+            <li>Counters</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <ul>
+            <li>Terms & Conditions</li>
+            <li>Privacy</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <ul>
+            <li>Crechterwoord K12 182 DK Alknjkcb</li>
+            <li>085-132567</li>
+            <li>info@payme.net</li>
+          </ul>
+        </div>
+      </div>
+      <div className="gpt3__footer-copyright">
+        <p>© 2021 GPT-3. All rights reserved</p>
+      </div>
     </div>
   )
 }
